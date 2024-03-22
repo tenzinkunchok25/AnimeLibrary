9054089f88b4da3a8cadd85d666d2e82164acd99
Tenzin Kunchok <tenzinkunchok012511@gmial.com>
2024-03-22 12:51:35 -0400

    ready to retrieve anime from api

--- js/script.js
+++ js/script.js
@@ -73,7 +73,7 @@ async function getTrendingAnimes(data) {
         let status = anime["status"];
         let genres = genresToString(anime["genres"]);
         let description = anime["description"];
-        let url = "./anime.html?anime=" + encodeURIComponent(title);
+        let url = "./anime.html?anime_id=" + encodeURIComponent(title);
 
         let poster = anime["bannerImage"];
         if (poster == null) {
@@ -117,7 +117,7 @@ async function getPopularAnimes(data) {
         let anime = data[pos];
         let title = anime["title"];
         let id = anime["id"];
-        let url = "./anime.html?anime=" + id;
+        let url = "./anime.html?anime_id=" + id;
         let image = anime["image"];
         let subOrDub;
         if (title.toLowerCase().includes("dub")) {
@@ -154,7 +154,7 @@ async function getRecentAnimes(page = 1) {
         let anime = data[pos];
         let title = anime["title"];
         let id = anime["id"].split("-episode-")[0];
-        let url = "./anime.html?anime=" + id;
+        let url = "./anime.html?anime_id=" + id;
         let image = anime["image"];
         let ep = anime["episode"].split(" ")[1];
         let subOrDub;
